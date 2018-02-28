f74fdb76f51a44f292fb58bce5283b55a57c4542
Murugan <murugu2012@gmail.com>
2018-02-28 15:43:52 +0530

    Validate exclusion of users.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :username, :exclusion => { :in => [ 'qwerty', 'abcdef' ]  }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
